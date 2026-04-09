326567d88d884e934edaf955d635891dc2509efa
Pravesh Kumar <aaravpravesh@live.com>
2026-04-09 06:17:28 +0530

    ROLE updated

--- src/App.jsx
+++ src/App.jsx
@@ -90,10 +90,10 @@ export default function App() {
                 <DashboardLayout>
                   {(() => {
                     const role = localStorage.getItem("user_role");
-                    if (role === "admin" || role === "superuser")
+                    if (role === "Admin" || role === "Superuser")
                       return <AdminHome />;
-                    if (role === "teacher") return <TeacherHome />;
-                    if (role === "student") return <StudentHome />;
+                    if (role === "Teacher") return <TeacherHome />;
+                    if (role === "Student") return <StudentHome />;
                     return <Login />; // Fallback
                   })()}
                 </DashboardLayout>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -45,11 +45,11 @@ export default function Login() {
         localStorage.setItem("user_name", data.full_name || data.username);
 
         // Role ke hisaab se redirect karein
-        if (data.role === "admin" || data.role === "superuser") {
+        if (data.role === "Admin" || data.role === "Superuser") {
           navigate("/admin-dashboard");
-        } else if (data.role === "teacher") {
+        } else if (data.role === "Teacher") {
           navigate("/admin-dashboard"); // Teacher bhi common dashboard par jayega
-        } else if (data.role === "student") {
+        } else if (data.role === "Student") {
           navigate("/admin-dashboard"); // Student bhi common dashboard par jayega
         }
       } else {
